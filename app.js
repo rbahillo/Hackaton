6ce38c7376c9f48d153f204e3eb409b6e8786198
Ricardo Bahillo <ricky.bahillo@gmail.com>
2014-04-11 15:33:02 +0200

    Add logic to receive the user result

--- app.js
+++ app.js
@@ -12,7 +12,20 @@ var path = require('path');
 //New Code
 var mongo = require('mongodb');
 var monk = require('monk');
-var db = monk('localhost:27017/test');
+//var db = monk('localhost:27017/test');
+
+var server = new mongo.Server("127.0.0.1", 27017, {});
+
+//obtenemos la base de datos de prueba que creamos
+var db = new mongo.Db('test', server, {})
+var collection;
+db.open(function (error, client) {
+	if (error) throw error;
+	collection = new mongo.Collection(client, 'usercollection');
+})
+
+
+	
 
 var app = express();
 
@@ -30,6 +43,7 @@ app.use(express.cookieParser('your secret here'));
 app.use(express.session());
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.bodyParser()); 
 
 
 /* Starts serving html */ 
@@ -39,6 +53,56 @@ app.get('/demo', function (req, res)
 {
     res.render('demo.html');
 });
+
+app.get('/Send', function(req, res){ // Specifies which URL to listen for
+	  req.body
+	  newstuff = [{ "alias" : "testuser1", "email" : "testuser3@testdomain.com" , "score" : "30"}]
+	  collection.find({email: newstuff[0].email}).toArray( function(err, results){
+		  user = results[0];
+		  if(user!=null){
+			  if(user.best<newstuff[0].score){
+				  user.best=newstuff[0].score
+			  }
+			  
+			  user.total=parseInt(user.total)+parseInt(newstuff[0].score)
+			  console.log(user);
+			  collection.save(user, {w: 1}, function(err, records){
+				   docs = collection.find().sort({best:1}).toArray(function(e,docs){
+				   res.render('userlist', {
+			            "userlist" : docs
+			        });
+				   });
+				    });
+		  }
+		  else{
+			  user = {}
+			  user.alias=newstuff[0].alias
+			  user.email=newstuff[0].email
+			  user.best=newstuff[0].score
+			  user.total=newstuff[0].score
+			  collection.insert(user, {w: 1}, function(err, records){
+				  docs = collection.find().sort({best:1}).toArray(function(e,docs){
+					   res.render('userlist', {
+				            "userlist" : docs
+				        });
+					   });
+					    });
+		  }
+			   
+			  
+	  });
+	  //var collection = new mongo.Collection(client, 'usercollection');
+	});
+
+app.get('/bestResults', function (req, res)
+		{
+			var collection = db.get('usercollection');
+			collection.find({},{},function(e,docs){
+		        res.render('userlist', {
+		            "userlist" : docs
+		        });
+		    });
+		});
 
 
 // development only
